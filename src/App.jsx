287eeb56d62009a983bb04e8e6558a26d8929819
David Forbes <davidforbesi@hotmail.com>
2023-12-12 16:47:35 +0000

    moved state from FlagContainer and passed down to children

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import Header from "./Header";
+import FlagContainer from "./FlagContainer";
+import { useState, useRef } from "react";
 import countriesArray from "./countriesArray";
-import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
   const [array, setArray] = useState(countriesArray);
+
+  const clickedFlags = useRef([]);
+
+  function reset() {
+    location.reload();
+  }
+
   function shuffleArray(array) {
     const arrayCopy = [...array];
     for (let i = arrayCopy.length - 1; i > 0; i--) {
@@ -13,20 +23,23 @@ function App() {
     return arrayCopy;
   }
 
-  function handleClick() {
-    setArray(shuffleArray(array));
-  }
-
   return (
-    <div className="countries">
-      {array.map(({ key, component }) => (
-        <div className="country" onClick={handleClick} key={key}>
-          {component()}
-          <p className="country-title">{key}</p>
+    <>
+      <Header score={score} />
+      {gameOver ? (
+        <div className="game-over-container">
+          <h1>Game Over</h1>
+          <button onClick={() => reset()}>Play Again</button>
         </div>
-      ))}
-    </div>
+      ) : (
+        <FlagContainer
+          addToScore={() => setScore(score + 1)}
+          gameOver={() => setGameOver(true)}
+          shuffleArray={() => setArray(shuffleArray(array))}
+          array={array}
+          clickedFlags={clickedFlags.current}
+        />
+      )}
+    </>
   );
 }
-
-export default App;
