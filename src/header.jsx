3ea23118803745e1c6b8bd294587d916bbf4aa22
David Forbes <davidforbesi@hotmail.com>
2023-12-12 15:30:10 +0000

    changed title and added a score prop and tag to display score

--- src/header.jsx
+++ src/header.jsx
@@ -1,3 +1,8 @@
-export default function Header() {
-  return <header></header>;
+export default function Header({ score = 0 }) {
+  return (
+    <header>
+      <h1>EU Country Memory Game</h1>
+      <h2>Score - {score} out of 27 </h2>
+    </header>
+  );
 }
