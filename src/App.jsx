815949709aeb02913d18b95cd1a87dcbf458942c
David Forbes <davidforbesi@hotmail.com>
2023-12-12 13:33:26 +0000

    added a function to shuffle the cards on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import countriesArray from "./countriesArray";
+import { useState } from "react";
 
 function App() {
+  const [array, setArray] = useState(countriesArray);
   function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
+    const arrayCopy = [...array];
+    for (let i = arrayCopy.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+      [arrayCopy[i], arrayCopy[j]] = [arrayCopy[j], arrayCopy[i]];
     }
-    return array;
+    return arrayCopy;
   }
-  const shuffledArray = shuffleArray([...countriesArray]);
+
+  function handleClick() {
+    setArray(shuffleArray(array));
+  }
+
   return (
     <div className="countries">
-      {shuffledArray.map(({ key, component }) => (
-        <div className="country" key={key}>
+      {array.map(({ key, component }) => (
+        <div className="country" onClick={handleClick} key={key}>
           {component()}
           <p className="country-title">{key}</p>
         </div>
