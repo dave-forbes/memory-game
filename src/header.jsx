b38c91e024c69a1027ddbcc858764b81f9e18748
David Forbes <davidforbesi@hotmail.com>
2023-12-13 10:48:02 +0000

    added hiscore state that changes base on score

--- src/header.jsx
+++ src/header.jsx
@@ -1,8 +1,17 @@
+import { useEffect, useState } from "react";
+
 export default function Header({ score = 0 }) {
+  const [hiScore, setHiScore] = useState(0);
+
+  useEffect(() => {
+    setHiScore((prevHiScore) => (score > prevHiScore ? score : prevHiScore));
+  }, [score, hiScore]);
+
   return (
     <header>
       <h1>EU Country Memory Game</h1>
       <h2>Score - {score} out of 27 </h2>
+      <h2>High Score - {hiScore} </h2>
     </header>
   );
 }
